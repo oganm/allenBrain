d7684dc55d28409744dafe1c4c0e4de21f9b96ad
OganM <ogan.mancarcii@gmail.com>
2017-05-02 14:29:52 -0700

    can operate without magick

--- R/allenBrain.R
+++ R/allenBrain.R
@@ -107,17 +107,18 @@ dowloadImage = function(imageID,outputFile = NULL,view = c('expression','project
 #' @param outputFile file path of the output. If null, a magick-image is returned
 #' @export
 centerImage = function(image, x ,y , xProportions = c(0.1,0.1), yProportions =c(0.1,0.1) , outputFile=NULL){
+    if ('magick' %in% (installed.packages() %>% rownames())){
     if(is.character(image)){
         image = magick::image_read(image)
     }
     dimensions = magick::image_info(image) %>% {c(.$width,.$height)}
-    
-    # dimensions = system(paste('identify',
-    #                           imageFile), intern = TRUE) %>%
-    #     regmatches(.,regexpr("(?<=[ ])[0-9]*?x[0-9]*?(?=[ ])",.,perl=T)) %>%
-    #     strsplit('x') %>% .[[1]] %>% as.double
-    
-    # image = jpeg::readJPEG(imageFile, native=TRUE) # old code to get the size
+    } else{
+        warning("Package 'magick' is not installed. Using system calls to imagemagick")
+        dimensions = system(paste('identify',
+                                  imageFile), intern = TRUE) %>%
+            regmatches(.,regexpr("(?<=[ ])[0-9]*?x[0-9]*?(?=[ ])",.,perl=T)) %>%
+            strsplit('x') %>% .[[1]] %>% as.double
+    }
 
     sizeX = (dimensions[1]*(xProportions[1] + xProportions[2])) %>% round
     sizeY = (dimensions[2]*(yProportions[1] +yProportions[2])) %>% round
@@ -128,13 +129,15 @@ centerImage = function(image, x ,y , xProportions = c(0.1,0.1), yProportions =c(
     beginningX = round(x) - shiftX
     beginningY = round(y) - shiftY
     
-    # system(paste0('convert "',imageFile, '" -crop ',sizeX,'x',sizeY,'+',beginningX,'+',beginningY,' "',outputFile,'"'))
-    
-    image = magick::image_crop(image,geometry = glue::glue('{sizeX}x{sizeY}+{beginningX}+{beginningY}'))
-    if (is.null(outputFile)){
-        return(image)
+    if ('magick' %in% (installed.packages() %>% rownames())){
+        image = magick::image_crop(image,geometry = glue::glue('{sizeX}x{sizeY}+{beginningX}+{beginningY}'))
+        if (is.null(outputFile)){
+            return(image)
+        } else{
+            magick::image_write(image,path = outputFile)
+        }
     } else{
-        magick::image_write(image,path = outputFile)
+        system(paste0('convert "',imageFile, '" -crop ',sizeX,'x',sizeY,'+',beginningX,'+',beginningY,' "',outputFile,'"'))
     }
 }
 
